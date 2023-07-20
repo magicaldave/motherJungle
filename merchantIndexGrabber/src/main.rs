f85f3462412331cefab2c7555a6a3472c60c59e6
Dave Corley <smokydave@icloud.com>
2023-07-20 15:17:10 -0500

    Edit: now also dumps leveled items

--- merchantIndexGrabber/src/main.rs
+++ merchantIndexGrabber/src/main.rs
@@ -9,13 +9,29 @@ use tes3::esp::*;
 #[global_allocator]
 static GLOBAL: mimalloc::MiMalloc = mimalloc::MiMalloc;
 
-#[derive(Default, Serialize)]
+#[derive(Debug, Serialize)]
+#[serde(untagged)]
+enum MerchantsOrLists {
+    Merchants(MerchantData),
+    LeveledLists(LeveledData),
+}
+
+#[derive(Debug, Default, Serialize)]
 struct MerchantData {
+    restock_minutes: u32,
     restocks_gold: bool,
     restocks_items: bool,
     restocks_containers: bool,
     gold_pool: u32,
     items: HashMap<String, i32>,
+}
+
+#[derive(Debug, Default, Serialize)]
+struct LeveledData {
+    is_list: bool,
+    list_flags: u32,
+    chance_none: u8,
+    items: HashMap<String, u16>,
 }
 
 fn main() -> std::io::Result<()> {
@@ -27,26 +43,38 @@ fn main() -> std::io::Result<()> {
     };
 
     // Mapping of { MerchantId => MerchantData }
-    let mut merchants = HashMap::<String, MerchantData>::new();
+    let mut data = HashMap::<String, MerchantsOrLists>::new();
 
-    for (restocking_inventories, _path) in plugins {
+    for (restocking_inventories, leveled_items, _path) in plugins {
         for restocking_inventory in restocking_inventories {
-            let merchent_data = merchants.entry(restocking_inventory.id).or_default();
 
-            merchent_data.restocks_items = true;
-            merchent_data.restocks_gold = true;
-            merchent_data.restocks_containers = true;
-            merchent_data.gold_pool = restocking_inventory.gold_pool;
+            let merchant_data = crate::MerchantData {
+                restock_minutes : 0,
+                restocks_items : true,
+                restocks_gold : true,
+                restocks_containers : true,
+                gold_pool : restocking_inventory.gold_pool,
+                items : convert_to_hashmap_npc(restocking_inventory.items),
+            };
+            data.insert(restocking_inventory.id, MerchantsOrLists::Merchants(merchant_data));
+        }
 
-            merchent_data.items.extend(restocking_inventory.items);
+        for leveled_item in leveled_items {
+            let entry = crate::LeveledData {
+                is_list : true,
+                list_flags : leveled_item.list_flags,
+                chance_none : leveled_item.chance_none,
+                items : convert_to_hashmap(leveled_item.items),
+            };
+            data.insert(leveled_item.id, MerchantsOrLists::LeveledLists(entry));
         }
     }
 
-    let serialized = serde_json::to_string_pretty(&merchants) //
+    let serialized_items = serde_json::to_string_pretty(&data) //
         .expect("Failed to serialize merchant data to JSON");
 
-    std::fs::write("merchantIndexDatabase.json", serialized) //
-        .expect("Merchant data failed to write");
+    std::fs::write("merchantIndexDatabase.json", serialized_items) //
+        .expect("LEVI data failed to write");
 
     Ok(())
 }
@@ -102,7 +130,34 @@ fn collect_restocking_inventories(plugin: Plugin) -> Vec<RestockingInventory> {
         .collect()
 }
 
-fn collect_plugins() -> Vec<(Vec<RestockingInventory>, PathBuf)> {
+fn collect_levelled_items(plugin: Plugin) -> Vec<LeveledItem> {
+    let mut items = vec![];
+
+    for levi in plugin.objects_of_type::<LeveledItem>() {
+        items.push(levi.clone());
+    }
+    items
+}
+
+fn convert_to_hashmap(items: Vec<(String, u16)>) -> HashMap<String, u16> {
+    let mut item_map = HashMap::<String, u16>::new();
+
+    for (item, count) in items {
+        item_map.insert(item, count);
+    }
+    item_map
+}
+
+fn convert_to_hashmap_npc(items: Vec<(String, i32)>) -> HashMap<String, i32> {
+    let mut item_map = HashMap::<String, i32>::new();
+
+    for (item, count) in items {
+        item_map.insert(item, count);
+    }
+    item_map
+}
+
+fn collect_plugins() -> Vec<(Vec<RestockingInventory>, Vec<LeveledItem>, PathBuf)> {
     std::fs::read_dir(".")
         .expect("Failed to read directory")
         .par_bridge()
@@ -124,12 +179,25 @@ fn collect_plugins() -> Vec<(Vec<RestockingInventory>, PathBuf)> {
                 return None;
             }
 
+            let mut levis = Plugin::new();
+            if levis
+                .load_path_filtered(&path, |tag| {
+                    matches!(&tag, LeveledItem::TAG)
+                })
+                .is_err()
+            {
+                println!("Failed to load {}", path.display());
+                return None;
+            }
+
+            let leveled_items = collect_levelled_items(levis);
+
             let restocking_inventories = collect_restocking_inventories(plugin);
             if restocking_inventories.is_empty() {
                 return None;
             }
 
-            Some((restocking_inventories, path))
+            Some((restocking_inventories, leveled_items, path))
         })
         .collect()
 }
